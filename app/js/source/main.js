1c70dab341f91be2c8460917736a50d37254154b
Mitchell Hollander <MitchellSH@live.com>
2014-03-24 13:45:15 -0500

    first commit to Tagstapic

---
+++ app/js/source/main.js
@@ -0,0 +1,56 @@
+(function(){
+
+  'use strict';
+
+  $(document).ready(initialize);
+
+  function initialize(){
+    $(document).foundation();
+    $('#search').click(showSearchBar);
+    $('#tagstapic').click(homeButton);
+    $('#bam').click(getPics);
+  }
+
+  //------Minor Show and Hide Functions--------
+
+  function showSearchBar(){
+    $('#container2').show();
+    $('#title').text('#Search Tags');
+  }
+
+  function homeButton(){
+    $('#title').text('#Tagstapic');
+    $('#container2').hide();
+    $('#target').empty();
+    //$('#search-box').val('');
+  }
+
+  //------Show and Hide Functions--------
+
+
+
+
+  //------Instagram API Functions--------
+
+
+  function getPics(){
+    var tag = $('#search-box').val();
+    var count = 6;
+    var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=86cecf102fdd46599736c4deac452424&callback=?&count=' + count;
+    $.getJSON(url, displayPics);
+  }
+
+  function displayPics(instaData){
+    var pics = instaData.data;
+    for(var i=0; i < pics.length; i++){
+      $('#target').append('<img src="'+pics[i].images.thumbnail.url+'">');
+    }
+  }
+
+  //------Instagram API Functions--------
+
+  
+  
+
+})();
+
