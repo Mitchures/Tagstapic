267923ba1701de21c36a4f2f23c248a2be999613
Mitchell Hollander <MitchellSH@live.com>
2014-03-25 16:03:26 -0500

    Added Create Slideshow page, made pictures appear in Slideshow div, etc..

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -9,23 +9,42 @@
     $('#search').click(showSearchBar);
     $('#tagstapic').click(homeButton);
     $('#bam').click(getPics);
+    $('.create').click(create);
   }
 
   //------Minor Show and Hide Functions--------
 
+  function create(){
+    $('#title').text('#Create Slideshow');
+    $('#container2').hide();
+    $('#container3').show();
+  }
+
   function showSearchBar(){
     $('#container2').show();
+    $('#container3').hide();
     $('#title').text('#Search Tags');
   }
 
   function homeButton(){
+    emptyPics();
     $('#title').text('#Tagstapic');
     $('#container2').hide();
-    $('#target').empty();
+    $('#container3').hide();
     $('#search-box').val('');
   }
 
+  function emptyPics(){
+    $('#target').empty();
+    $('#APIslideshow').empty();
+  }
   //------Show and Hide Functions--------
+
+  //------Test Functions-----------------
+
+  
+
+  //------Test Functions-----------------
 
 
 
@@ -34,7 +53,7 @@
 
 
   function getPics(){
-    $('#target').empty();
+    emptyPics();
     var tag = $('#search-box').val();
     var count = 6;
     var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=86cecf102fdd46599736c4deac452424&callback=?&count=' + count;
@@ -45,6 +64,7 @@
     var pics = instaData.data;
     for(var i=0; i < pics.length; i++){
       $('#target').append('<img src="'+pics[i].images.thumbnail.url+'">');
+      $('#APIslideshow').append('<img src="'+pics[i].images.thumbnail.url+'">');
     }
   }
 
